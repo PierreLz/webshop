7681fe168317a7d27598dc59bd36113659dade62
Pierre Laroze <pierre.laroze@live.fr>
2019-03-24 19:43:34 +0100

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+
+5.times do
+  Product.create(title: 'Shaker',
+  description: %{Native shaker, multiformat.It was be the on for you.},
+    image_url: "https://res.cloudinary.com/bistro-mimi/image/upload/v1526314876/sample.jpg",
+    price: 10.50)
+end
+
